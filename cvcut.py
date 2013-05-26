2b572b99bbcc5408358bad79d91c1993b1e404cf
James McMurray <jamesmcm03@gmail.com>
2013-05-26 01:11:02 +0000

    Updated for new test

--- cvcut.py
+++ cvcut.py
@@ -5,33 +5,44 @@ import pickle
 from scipy.misc import imresize
 np.set_printoptions(threshold='nan')
 pixelthreshold=0 #for sum method
-dsize=(70,50)
-esize=70*50
+#dsize=(70,50)
+#esize=70*50
+
+dsize=(40,30)
+esize=dsize[0]*dsize[1]
+
 
 def extractdigits(filename):
     a=cv2.imread(filename, 0)
-
-    cv2.namedWindow("newtest")
+    #print a.shape
+    a=a[:,1000:1450]
+    #cv2.namedWindow("newtest")
     #cv2.imshow("newtest",a)
     a=cv2.GaussianBlur(a,(3,3), 0)
+    orig=a.copy()
+    #(thresh, a) = cv2.threshold(a, 80, 255, cv2.THRESH_BINARY)
 
-    (thresh, a) = cv2.threshold(a, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    #(thresh, a) = cv2.threshold(a, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
 
-    #a=cv2.adaptiveThreshold(a, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, 3)
+    a=cv2.adaptiveThreshold(a, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 17, 2)
+
     #a=cv2.bitwise_not(a)
 
-    #cv2.imshow("newtest",a)
-    #cv2.waitKey(0)
+    cv2.imshow("newtest",a)
+    cv2.waitKey(0)
+    #sys.exit()
     #raw_input()
 
     b=a.copy()
 
     contours, hierarchy = cv2.findContours(a, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_KCOS)
-    #mask = np.zeros(a.shape, dtype=np.uint8)
+    mask = np.zeros(a.shape, dtype=np.uint8)
     #cv2.drawContours(mask, contours, 0, 100)
     #cv2.drawContours(mask, contours, 1, 100)
     #cv2.drawContours(mask, contours, 2, 100)
+    #cv2.imshow("newtest",mask)
+    #cv2.waitKey(0)
     dlist=[]
     output=np.zeros(b.shape,dtype=np.uint8)
 
@@ -39,28 +50,68 @@ def extractdigits(filename):
 
         br=cv2.boundingRect(cont)
         #print br
-        #cv2.rectangle(b, (br[0],br[1]), (br[0]+br[2],br[1]+br[3]), 255)
+        cv2.rectangle(b, (br[0],br[1]), (br[0]+br[2],br[1]+br[3]), 255)
         charray=np.zeros(dsize, dtype=np.uint8)
         temp=b[br[1]:br[1]+br[3], br[0]:br[0]+br[2]]
-        if (temp.shape[0]>10 and temp.shape[0]<=dsize[0] and temp.shape[1]>10 and temp.shape[1]<=dsize[1]):
+        #cv2.imshow("newtest",temp)
+        #cv2.waitKey(0)
+
+        if (temp.shape[0]>0.9*dsize[0] and temp.shape[0]<=2*dsize[0] and temp.shape[1]>0.9*dsize[1] and temp.shape[1]<=5*dsize[1]):
+            #print temp.shape
             #charray[:temp.shape[0],:temp.shape[1]]=temp
-            charray=temp.copy()
-            charray=imresize(charray, dsize)
-            dlist.append(charray)
+            #(thresh, temp) = cv2.threshold(temp, 100, 255, cv2.THRESH_BINARY)
+            temp=cv2.bitwise_not(temp)
+            temp2=temp.copy()
+            cv2.imshow("newtest",temp)
+            cv2.waitKey(0)
+            #contours2, hierarchy = cv2.findContours(temp2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_KCOS)
+            contours2, hierarchy = cv2.findContours(temp2, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+            for cont2 in contours2:
+                br2=cv2.boundingRect(cont2)
+                # temp3=temp[br2[1]:br2[1]+br2[3], br2[0]:br2[0]+br2[2]]
+                # cv2.imshow("newtest", temp3)
+                # print temp3.shape
+                # cv2.waitKey(0)
+
+
+                if br2[3]<dsize[0]+10 and br2[3]>dsize[0]-10 and br2[2]<dsize[1]+10 and br2[2]>dsize[1]-20 and br2[0]>0+(temp.shape[1]/20) and br2[0]<temp.shape[1]-(temp.shape[1]/5):
+                    mask = np.zeros(temp2.shape, dtype=np.uint8)
+                    cv2.drawContours(mask,[cont2],0,255,-1)
+                    temp2=temp.copy()
+                    temp2[mask==0]=0
+
+                    temp3=temp2[br2[1]:br2[1]+br2[3], br2[0]:br2[0]+br2[2]]
+                    charray=temp3.copy()
+                    charray=imresize(charray, dsize)
+                    dlist.append(charray)
+
+
+                    print temp3.shape
+                    cv2.imshow("newtest", temp3)
+                    cv2.waitKey(0)
+            
+                #if br2[2]>10 and br2[3]>10:
+                    #cv2.rectangle(temp, (br2[0],br2[1]), (br2[0]+br2[2],br2[1]+br2[3]), 255)
+
+
+            
+            # charray=temp.copy()
+            # charray=imresize(charray, dsize)
+            # dlist.append(charray)
             #output[br[1]:br[1]+br[3], br[0]:br[0]+br[2]]=b[br[1]:br[1]+br[3], br[0]:br[0]+br[2]]
             #cv2.rectangle(output, (br[0],br[1]), (br[0]+br[2],br[1]+br[3]), 255)
         #print temp.shape
         #dlist=dlist[1:-1]
     #cv2.imwrite("readout.png", output)
-    #sys.exit()
-    #for item in dlist:  
-        #dlist[0].resize((70,50))
-        #cv2.imshow("newtest",item)
-        #cv2.waitKey(0)
-        #print item.shape
-        #raw_input()
+    #cv2.imshow("newtest",b)
+    #cv2.waitKey(0)
 
-    #sys.exit()
+    # for item in dlist:  
+    #     dlist[0].resize(dsize)
+    #     cv2.imshow("newtest",item)
+    #     cv2.waitKey(0)
+    #     print item.shape
+        #raw_input()
     return dlist
 
 # ML part: scikit training
@@ -142,6 +193,45 @@ def sumpmtrain(dlist, sols, classifierfile):
     pickle.dump(tdict, f)
     f.close()
 
+def sumpmtrain2(dlist, classifierfile):
+    #Summing pixel match train:
+    #Make maximum value=1
+    #flatten data in to one array with one row per image
+    fileexists=True
+    try:
+        f=open(classifierfile,"r")
+        tdict=pickle.load(f)
+        f.close()
+    except:
+        tdict={}
+        fileexists=False
+
+
+    for item in dlist:
+        data = np.zeros((esize), dtype=np.uint8)
+        data=item.flatten()
+        vecfunc=np.vectorize(binarise)
+        data=vecfunc(data)
+        cv2.imshow("newtest2",item)
+        ci=cv2.waitKey(0)
+        #ci=raw_input()
+        ci=ci-48
+        if ci in [0,1,2,3,4,5,6,7,8,9]:
+            if fileexists==False:
+                tdict[ci]=data
+        
+            elif fileexists==True:
+                if ci in tdict.keys():
+                    tdict[ci]+=data
+                    tdict[ci]/=2.0
+                else:
+                    tdict[ci]=data
+
+    f=open(classifierfile,"w")
+    pickle.dump(tdict, f)
+    f.close()
+
+
 def bool2int(x):
     if x==True:
         return 1
@@ -197,6 +287,40 @@ def sumpmtest(classifierfile, dlist):
 
     #print data[0]
 
+def sumpmtest2(classifierfile, dlist):
+    f=open(classifierfile,"r")
+    tdict=pickle.load(f)
+    f.close()
+
+    for item in dlist:
+        data = np.zeros((esize), dtype=np.uint8)
+    #results=[]
+    #err=[]
+        data=item.flatten()
+        boolarray=(data>pixelthreshold)
+        resultd=[]
+        print tdict.keys()
+        for j in range(len(tdict.keys())):
+            result=np.sum(tdict[j][boolarray])
+            #penalisation factor
+            result-=4*np.sum(tdict[j][data<=pixelthreshold])
+            resultd.append(result/float(esize))
+        print resultd
+        # sr=reversed(sorted(resultd))
+        # srlist=[]
+        # for j in sr:
+        #     srlist.append(j)
+        # err.append(srlist[0]-srlist[1])
+        resultf=(resultd.index(max(resultd)))
+        print resultf
+        if max(resultd)<-0.1:
+            print "IGNORE!"
+
+        print "---"
+        cv2.imshow("newtest",item)
+        cv2.waitKey(0)
+
+
 def plotsumpkl(tdictfile):
     f=open(tdictfile,"r")
     tdict=pickle.load(f)
@@ -215,12 +339,13 @@ if __name__=="__main__":
     #main here
     # dlist=extractdigits("data/test1.png")
     # sols=np.array([5,2,1,0,7,4,3,8,6,9])
-    # sumpmtrain(dlist, sols, "tdictsum.pkl")
+    
     # dlist=extractdigits("data/test2.png")
     # sols=np.array([2,1,0,7,5,4,3,9,8,6])
     # sumpmtrain(dlist, sols, "tdictsum.pkl")
-    dlist=extractdigits("data/test3.png")
-    sumpmtest("tdictsum.pkl", dlist)
+    dlist=extractdigits("data/camtest7.png")
+    #sumpmtrain2(dlist, "tdictsumnew.pkl")
+    sumpmtest2("tdictsumnew.pkl", dlist)
     #plotsumpkl("tdictsum.pkl")
     #pmtrain(dlist, sols, "tdict.pkl")
     #pmtest("tdict.pkl", dlist)
